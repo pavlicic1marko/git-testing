b2a55681cfa12c33f02b7ecffb714a992de864ca
Marko Pavlicic <marko.pavlicic@computerrock.com>
2024-08-10 11:27:12 +0200

    testinggg

--- src/Learn1.js
+++ src/Learn1.js
@@ -8,3 +8,21 @@ else
 {
     console.log("condition is not satisfied")
 }
+
+let i = 0
+while(i < 10)
+{
+    i++
+    console.log("i am in a loop: " , i)
+}
+
+
+for(let k=22; k<=33; k++)
+{
+    if(k%2==0 || k%5 ==0){
+        console.log("is eaven and mod of 5")
+        console.log("break now")
+        break
+    }
+    console.log(k)
+}
