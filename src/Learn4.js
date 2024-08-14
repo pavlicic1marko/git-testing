ed9a1b9dcf88f0d51b70cd0e367ad24c41419d59
Marko Pavlicic <marko.pavlicic@computerrock.com>
2024-08-14 09:37:01 +0200

    learning

---
+++ src/Learn4.js
@@ -0,0 +1,17 @@
+//block of code 
+const const1 = 123
+let day ='12334534534'
+
+function addNumbers(a,b){
+    return a + b
+}
+let sum = addNumbers(1,2)
+
+let sumOfnumbers = (c,d) => c+d
+console.log(sum)
+console.log(sumOfnumbers(1,33))
+console.log(day.length)
+
+let splitArray = day.split('4')
+console.log(splitArray)
+
