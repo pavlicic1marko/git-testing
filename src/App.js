71c8674e7d318ff928122ca40524e2485e160ada
Marko Pavlicic <marko.pavlicic@computerrock.com>
2024-09-25 13:27:37 +0200

    Squashed commit of the following:
    commit bdac452cfdd5b5085188b9c6e3e006dde60c2139
    Author: Marko Pavlicic <marko.pavlicic@computerrock.com>
    Date:   Wed Sep 25 11:40:37 2024 +0200
    
        New commit message
    
    commit 2d948852dc7c3e14ec5e70272a66d4c8ad27b34a
    Author: Marko Pavlicic <marko.pavlicic@computerrock.com>
    Date:   Wed Sep 25 11:30:22 2024 +0200
    
        coommit 2
    
    commit 9191bac0e5f1fe904f60bbeacca1749feb6ee8e0
    Author: Marko Pavlicic <marko.pavlicic@computerrock.com>
    Date:   Wed Sep 25 11:20:03 2024 +0200
    
        test123

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import {Container} from 'react-bootstrap'
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 
 //test123
+//commmit 1234567890
+//commit3
 function App() {
   return (
     <Router>
