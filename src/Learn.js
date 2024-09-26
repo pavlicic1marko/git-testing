8cf9487c33316bdc847313150345589440685a0f
Marko Pavlicic <marko.pavlicic@computerrock.com>
2024-09-26 11:48:27 +0200

    edit file

--- src/Learn.js
+++ src/Learn.js
@@ -7,8 +7,9 @@ var a = 4
 let b = 4
 var ab = 1
 var ab = a + b
-console.log(ab)
-console.log(typeof(a))
-console.log(!true)
+console.log(a)
+console.log(typeof(ab))
+console.log(true)
+console.log("true")
 
 ///uku
