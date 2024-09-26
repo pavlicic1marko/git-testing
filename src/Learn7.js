2d256d02c11795c32cf8a6b4611749a53d96d1b0
Marko Pavlicic <marko.pavlicic@computerrock.com>
2024-09-26 17:00:59 +0200

    commit changes to branch 3

--- src/Learn7.js
+++ src/Learn7.js
@@ -1,5 +1,7 @@
 const Person = require('./Learn6')
-var person1 = new Person("jhon","whick")
+var person1stupidname  = new Person("jhon","whick")
 var person2 = new Person("jhon","whick")
+var person4 = new Person("jhon","whick")
+
 console.log(person1.fullName())
 ///bla bla bla
